6febf393b24aff3e10df4babce9bc627455b1a5e
Milton SHerrera <mlton.s.herrera@live.com>
2026-02-20 15:03:20 -0500

    Elimina imágenes Hankook.jpg, Momo.jpg y maxxis-logo.jpg del directorio de productos

--- Assets/Js/Product_Gallery_Data.js
+++ Assets/Js/Product_Gallery_Data.js
@@ -1,15 +1,11 @@
 const PRODUCT_IMAGES = [
     "Rockford_Fosgate.jpg",
     "Bridgestone.jpg",
-    "Hankook.jpg",
     "Narva.jpg",
     "vossen.jpg",
     "bfgoodrich.jpg",
     "Blacklion.jpg",
     "Dunlop.jpg",
-    "Hankook.jpg",
-    "maxxis-logo.jpg",
     "Michelin.jpg",
-    "Momo.jpg",
     "Pirelli.jpg"
 ];
